2d6b8d114cebec8494772e42f1233b8438c9d8e0
SOFKA\maricela.coral <mcoralsk@rapicredit.com>
2023-02-23 23:38:08 -0500

    first commit

---
+++ src/main/java/com/sofka/serviciosRest/retoPracticoServiciosRest/RetoPracticoServiciosRestApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.serviciosRest.retoPracticoServiciosRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoPracticoServiciosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoPracticoServiciosRestApplication.class, args);
+	}
+
+}
